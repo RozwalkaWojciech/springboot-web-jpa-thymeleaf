28b574708940e9a324bf32c96965e9e16eeac802
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-29 21:23:31 +0100

    init commit

---
+++ src/main/java/spring/springbootwebjpathymeleaf/SpringbootWebJpaThymeleafApplication.java
@@ -0,0 +1,13 @@
+package spring.springbootwebjpathymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebJpaThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebJpaThymeleafApplication.class, args);
+	}
+
+}
