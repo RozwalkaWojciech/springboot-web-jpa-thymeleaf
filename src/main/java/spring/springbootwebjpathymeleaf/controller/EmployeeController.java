691fb508ae7ea175c22c013a3a89a9787242f7f9
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-30 12:28:22 +0100

    add link to control new employee

--- src/main/java/spring/springbootwebjpathymeleaf/controller/EmployeeController.java
+++ src/main/java/spring/springbootwebjpathymeleaf/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import spring.springbootwebjpathymeleaf.model.Employee;
 import spring.springbootwebjpathymeleaf.service.EmployeeService;
 
 @Controller
@@ -12,9 +13,16 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping( "/")
+    @GetMapping("/")
     public String viewHomePage(Model model) {
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
         return "index";
+    }
+
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "new_employee";
     }
 }
