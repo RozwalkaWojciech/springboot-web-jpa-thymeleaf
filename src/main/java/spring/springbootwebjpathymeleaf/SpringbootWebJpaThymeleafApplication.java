1bbe01ffa3ff2db1f8a18707ad84226ec6f4d3e0
Wojciech <wojciechrozwalka@gmail.com>
2021-01-02 18:53:46 +0100

    clean code

--- src/main/java/spring/springbootwebjpathymeleaf/SpringbootWebJpaThymeleafApplication.java
+++ src/main/java/spring/springbootwebjpathymeleaf/SpringbootWebJpaThymeleafApplication.java
@@ -1,14 +1,13 @@
 package spring.springbootwebjpathymeleaf;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootWebJpaThymeleafApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWebJpaThymeleafApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebJpaThymeleafApplication.class, args);
+    }
 
 }
